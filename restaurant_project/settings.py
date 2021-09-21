35e2b6894fd3e257f7e4e6e8a36fe0d8e606b8a9
evgeny <evgeny_perchun@mail.ru>
2021-09-21 15:21:49 +0300

    remove unnecessary requirements

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third party apps
     "rest_framework",
-    "django_filters",
     "djoser",
     "drf_yasg",
     # First party apps
@@ -104,9 +103,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
